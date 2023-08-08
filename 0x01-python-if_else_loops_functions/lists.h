3143744858f08b7a7938d9bbbb3d4ccf50514ce3
alvinkgbateh <alvingbatehsl@gmail.com>
2023-08-08 15:34:47 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,7 +1,5 @@
-ifndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
-
-#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -13,10 +11,9 @@ ifndef LISTS_H
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
-
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
