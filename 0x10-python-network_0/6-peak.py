faeaa87d04cbe6e61870071069824dde3736b9fd
alvinkgbateh <alvingbatehsl@gmail.com>
2023-12-03 11:11:20 -0800

    Python - Network #0

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module solves the following requirements and relies on the file
+    test/6-main.py
+
+Write a function that finds a peak in a list of unsorted integers.
+    - Prototype: def find_peak(list_of_integers):
+    - You are not allowed to import any module
+    - Your algorithm must have the lowest complexity (hint: you don't need to
+      go through all numbers to find a peak)
+    - 6-peak.py must contain the function
+    - 6-peak.txt must contain the complexity of your algorithm: O(log(n)),
+      O(n), O(nlog(n)) or O(n2)
+    - Note: there may be more than one peak in the list
+"""
+
+
+def find_peak(list_of_integers):
+    """ Find a peak in an unsorted list of integers """
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,25 @@
+Python - Network #0
+Resources
+MDN HTTP Headers
+HTTP (HyperText Transfer Protocol)
+MDN HTTP cookies
+Curl Command in Linux with Examples
+man curl
+Learning Objectives
+What a URL is
+What HTTP is
+How to read a URL
+The scheme for a HTTP URL
+What a domain name is
+What a sub-domain is
+How to define a port number in a URL
+What a query string is
+What an HTTP request is
+What an HTTP response is
+What HTTP headers are
+What the HTTP message body is
+What an HTTP request method is
+What an HTTP response status code is
+What an HTTP Cookie is
+How to make a request with cURL
+What happens when you type google.com in your browser (Application level)
